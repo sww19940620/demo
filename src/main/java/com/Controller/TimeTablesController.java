2640473046f71c96f431ab7499523dac7374a5a9
sww19940620 <616381714@qq.com>
2019-01-16 15:43:55 +0800

    111

---
+++ src/main/java/com/Controller/TimeTablesController.java
@@ -0,0 +1,28 @@
+package com.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="/TimeTables")
+public class TimeTablesController {
+
+    @RequestMapping(value = "/get",method = RequestMethod.GET )
+    public String getTimeTables(@RequestParam(name="userName")String userName){
+
+        String courese ="[" +
+                "['大学英语(Ⅳ)@10203', '大学英语(Ⅳ)@10203', '', '', '', '', '毛概@14208', '毛概@14208', '', '', '', '选修']," +
+                "['', '', '信号与系统@11302', '信号与系统@11302', '模拟电子技术基础@16204', '模拟电子技术基础@16204', '', '', '', '', '', '']," +
+                "['大学体育(Ⅳ)', '大学体育(Ⅳ)', '形势与政策(Ⅳ)@15208', '形势与政策(Ⅳ)@15208', '', '', '电路、信号与系统实验', '电路、信号与系统实验', '', '', '', '']," +
+                "['', '', '', '', '电装实习@11301', '电装实习@11301', '', '', '', '大学体育', '大学体育', '']," +
+                "['', '', '数据结构与算法分析', '数据结构与算法分析', '', '', '', '', '信号与系统', '信号与系统', '', '']," +
+                "['大学英语(Ⅳ)@10203', '大学英语(Ⅳ)@10203', '', '', '', '', '毛概@14208', '毛概@14208', '', '', '', '选修']" +
+                "]";
+
+        return courese;
+    }
+}
