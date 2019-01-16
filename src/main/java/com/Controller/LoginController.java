a21ed772a5ab03ff0d51cab5a3617a92ed73ca72
sww19940620 <616381714@qq.com>
2019-01-16 15:51:07 +0800

    111

--- src/main/java/com/Controller/LoginController.java
+++ src/main/java/com/Controller/LoginController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
 
 
-    @RequestMapping(value = "/check",method = RequestMethod.POST,consumes="application/json;charset=UTF-8" )
+    @RequestMapping(value = "/check",method = RequestMethod.POST )
     public int LoginCheck(@RequestBody LoginInfo loginInfo){
-        System.out.println("121212121212"+loginInfo.getUserName());
         String userName =loginInfo.getUserName();
         String password =loginInfo.getPassword();
         if(userName.equals("123456")&&password.equals("123456")){
